be0ac9422adb571a4cde771825dc493e0a0e6a28
Arturo Rinaldi <arty.net2@gmail.com>
2016-01-11 14:41:11 +0100

    Linino 04 : adding TOPDIR files and scripts

---
+++ scripts-custom/install_all_modules.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ -z $1 ]
+then
+	echo ""
+	echo "Insert a directory !"
+	echo ""
+	
+	exit 0
+fi
+
+cd $1
+
+if [ -z `ls package/linino | grep node- | head -n 1` ]
+then
+	echo "nothing !"
+	exit 0
+fi 
+
+for p in `ls package/linino/ | grep node-`
+do 
+	make -j4 package/linino/$p/compile V=99 && echo "" && sleep 2
+	
+done
+
+if [ $? -eq 1 ]
+then
+	exit 0
+fi 
+
+make package/index V=99
