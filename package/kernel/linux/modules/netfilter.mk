eb16e707c36b40274666628f9a1cf2b9ed915a1a
Arturo Rinaldi <arty.net2@gmail.com>
2016-08-05 09:58:17 +0200

    new package : kmod-br-netfilter

--- package/kernel/linux/modules/netfilter.mk
+++ package/kernel/linux/modules/netfilter.mk
@@ -578,7 +578,7 @@ define KernelPackage/ipt-extra
   KCONFIG:=$(KCONFIG_IPT_EXTRA)
   FILES:=$(foreach mod,$(IPT_EXTRA-m),$(LINUX_DIR)/net/$(mod).ko)
   AUTOLOAD:=$(call AutoProbe,$(notdir $(IPT_EXTRA-m)))
-  $(call AddDepends/ipt)
+  $(call AddDepends/ipt, +kmod-br-netfilter)
 endef
 
 define KernelPackage/ipt-extra/description
@@ -642,6 +642,17 @@ endef
 
 $(eval $(call KernelPackage,arptables))
 
+define KernelPackage/br-netfilter
+  SUBMENU:=$(NF_MENU)
+  TITLE:=Bridge netfilter support modules
+  HIDDEN:=1
+  DEPENDS:=+kmod-ipt-core +kmod-bridge
+  FILES:=$(LINUX_DIR)/net/bridge/br_netfilter.ko
+  KCONFIG:=CONFIG_BRIDGE_NETFILTER
+  AUTOLOAD:=$(call AutoProbe,br_netfilter)
+endef
+
+$(eval $(call KernelPackage,br-netfilter))
 
 define KernelPackage/ebtables
   SUBMENU:=$(NF_MENU)
